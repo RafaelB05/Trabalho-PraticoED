cef94b35b202268c8eeeecbdfa35919abcb7b08a
Rafael Brunini Pereira <bruninirafael@gmail.com>
2022-07-08 01:32:09 -0300

    Funcao pra inserir

--- inserirNovoRegistro.cpp
+++ inserirNovoRegistro.cpp
@@ -19,11 +19,24 @@ struct Call_911
 };
 
 void inserirNovoRegistro(int posicaoInsercao){
-    ofstream paste;
     
+    ifstream paste;
+    paste.open("paste.bin", std::ios::binary);
+    paste.seekg(0, paste.end);
+    int tam = paste.tellg();
+    paste.seekg(0, paste.beg);
+    int numero_registros = tam / sizeof(Call_911);
 
+    Call_911 *registros = new Call_911[1];
 
-
+    for(int i = numero_registros; i > posicaoInsercao;i--)
+    {
+        paste.seekg(i * sizeof(Call_911),paste.beg);
+        paste.read((char*)(&registros[0]), sizeof(Call_911));
+        cout << registros[0].id << endl;
+        //paste.seekg(i*sizeof(Call_911),paste.beg);
+        //paste.write((const char *)(&registros[0]),sizeof(Call_911)); 
+    }
 }
 
 
@@ -32,7 +45,6 @@ int main(){
     int posicaoInsercao;
 
     cin >> posicaoInsercao;
-
     inserirNovoRegistro(posicaoInsercao);
 
     return 0;
