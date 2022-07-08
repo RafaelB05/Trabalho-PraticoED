b6be0ca931520f4245af77dc6f4d49cdab77e0d5
K3yNet <53867555+K3yNet@users.noreply.github.com>
2022-07-08 16:49:51 -0300

    Update inserirNovoRegistro.cpp

--- inserirNovoRegistro.cpp
+++ inserirNovoRegistro.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 struct Call_911
 {
-	unsigned int id;
+	int id;
 	float lat;
 	float lgn;
 	char desc[120];
@@ -16,14 +16,17 @@ struct Call_911
 	char twp[40];
 	char addr[75];
 	int e;
+	bool flag;
 };
 
-void inserirNovoRegistro(int posicaoInsercao)
+void inserirNovoRegistro(int posicaoInsercao, Call_911 New)
 {
     //331740
     //330000
-    ifstream paste;
-    paste.open("paste.bin", std::ios::binary);
+
+    fstream paste;
+	paste.open("paste.bin",fstream::in | fstream::out | fstream::binary);
+
     paste.seekg(0, paste.end);
     int tam = paste.tellg();
     paste.seekg(0, paste.beg);
@@ -31,12 +34,30 @@ void inserirNovoRegistro(int posicaoInsercao)
 
     Call_911 *registros = new Call_911[1];
 
-		for ( int i = numero_registros - 1; i > posicaoInsercao - 1; i-- )
-		{
-			paste.seekg(i * sizeof(Call_911), paste.beg);
-			paste.read((char*)(&registros[0]), sizeof(Call_911));	
-			cout << registros[0].id << endl;
-        }
+	for ( int i = numero_registros - 1; i >= posicaoInsercao; i-- )
+	{
+		paste.seekg(i * sizeof(Call_911), paste.beg);
+		paste.read((char*)(&registros[0]), sizeof(Call_911));
+		registros[0].id += 1;
+		cout << registros[0].id << endl;
+		cout << registros[0].lat << endl;
+		cout << registros[0].lgn << endl;
+		cout << registros[0].desc << endl;
+		cout << registros[0].zip << endl;
+		cout << registros[0].title << endl;
+		cout << registros[0].timeStamp << endl;
+		cout << registros[0].twp << endl;
+		cout << registros[0].addr << endl;
+		cout << registros[0].e << endl;
+		cout << "- - - - - - - - - - - - " << endl;
+		paste.seekp((i + 1) * sizeof(Call_911), paste.beg);
+		paste.write((const char *) (&registros[0]), sizeof(Call_911));
+	}
+	New.id = posicaoInsercao;
+	paste.seekp(posicaoInsercao * sizeof(Call_911), paste.beg);
+	paste.write((const char *) (&New), sizeof(Call_911));
+
+	paste.close();
 }
 
 
@@ -45,8 +66,23 @@ int main()
 
     int posicaoInsercao;
 
-    cin >> posicaoInsercao;
-    inserirNovoRegistro(posicaoInsercao);
+	Call_911 New;
+
+	cin >> posicaoInsercao;
+
+	cin >> New.lat;
+	cin >> New.lgn;
+	//cin >> New.desc;
+	cin >> New.zip;
+	//cin >> New.title;
+	//cin >> New.timeStamp;
+	//cin >> New.twp;
+	//cin >> New.addr;
+	cin >> New.e;
+	New.flag = true;
+
+	cout << endl;
+    inserirNovoRegistro(posicaoInsercao, New);
 
     return 0;
 }
